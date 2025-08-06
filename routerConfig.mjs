13a5504aa2950ef65fd27b440fe7f15c8f1f2dd9
varadan13 <rcaesar1996@gmail.com>
2025-08-06 15:16:02 +0530

    restructure

--- routerConfig.mjs
+++
@@ -1,4 +0,0 @@
-export const routerConfig = {
-    '/moving-git-branch-to-new-commit': () => import('./blog/moving-git-branch-to-new-commit.js'),
-    '/single-page-app-hack-github-pages': () => import('./blog/single-page-app-hack-github-pages.js')
-}
