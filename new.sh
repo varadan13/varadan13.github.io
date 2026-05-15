f50b876e9c030833f14f3b272caedac26a60d975
varadan13 <rcaesar1996@gmail.com>
2026-05-15 21:39:45 +0530

    add og tags

--- new.sh
+++ new.sh
@@ -45,6 +45,13 @@ cat > "$SHELL_FILE" <<HTML
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>$TITLE</title>
+    <meta name="description" content="" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="https://varadan13.github.io/$SHELL_DIR/$slug.html" />
+    <meta property="og:title" content="$TITLE" />
+    <meta property="og:description" content="" />
+    <meta property="og:image" content="https://varadan13.github.io/assets/header-image.jpg" />
+    <meta property="og:site_name" content="varadan13.github.io" />
 
     <link rel="icon" type="image/png" href="../favicon/" sizes="96x96" />
     <link rel="icon" type="image/svg+xml" href="../favicon/favicon.svg" />
